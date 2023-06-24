44d4f65a7406f7ed190fd1c5371a7432f7a288e7
Sebastien <sebdh@hotmail.com>
2023-06-23 23:56:37 -0600

    added list of videos and usestate to change selected video and update list. Added styling for list

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Nav from "./components/Nav/Nav";
 import "./styles/partials/Resets.scss";
 import SelectedVideo from "./components/SelectedVideo/SelectedVideo";
 import videoData from "./data/video-details.json";
-
+import SelectedVideoInformation from "./components/SelectedVideoInformation/SelectedVideoInformation";
+import VideoList from "./components/VideoList/VideoList";
 
 function App() {
 const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
 
-  return (
+const clickHandler = (video) => {
+  setSelectedVideo(video)
+};
+
+return (
     <div className="App">
       <Nav />
       <SelectedVideo video={selectedVideo}/>
+      <SelectedVideoInformation video={selectedVideo} />
+
+      <VideoList data={videoData} video={selectedVideo} clickHandler={clickHandler}/>
     </div>
   );
 }
---
+++ src/Components/VideoList/VideoList.js
@@ -0,0 +1,40 @@
+import "./VideoList.scss";
+
+const VideoList = (props) => {
+  const filterVideo = props.data.filter((ele) => {
+    return ele.id !== props.video.id;
+  });
+
+  return (
+    <ul className="video__list">
+    <p className="nextVideos">NEXT VIDEOS</p>
+      {filterVideo.map((ele) => {
+        return (
+          <li
+            onClick={() => {
+              props.clickHandler(ele);
+            }}
+            key={ele.id}
+            className="video__list-item"
+          >
+            <div className="video__list-item-image-container">
+              <img
+                className="video__list-item-image"
+                src={ele.image}
+                alt={ele.title}
+              ></img>
+            </div>
+            <div>
+              <p className="video__list-item-text video__list-item-text--bold">
+                {ele.title}
+              </p>
+              <p className="video__list-item-text">{ele.channel}</p>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default VideoList;
